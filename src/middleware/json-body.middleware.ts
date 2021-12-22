e23917c6a13460fd164c84aa95feec59bdc7bc86
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-12-22 17:42:17 +0100

    Removed logging

--- src/middleware/json-body.middleware.ts
+++ src/middleware/json-body.middleware.ts
@@ -1,20 +1,17 @@
-import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 
 
 // ugly solution I had to put in place because express' json parser was not calling next on /twitch/*
+// TODO: find more efficient way to implement
 @Injectable() 
 export class JSONBodyMiddleware implements NestMiddleware {
-    private readonly logger = new Logger(JSONBodyMiddleware.name);
-    
     use(req: any, res: any, next: () => void) {
         const originalBody = req.body;
         try {
             req.body = JSON.parse(req["rawBody"]);
         } catch (err) {
             req.body = originalBody;
-            this.logger.log(`Could not parse JSON for ${req.path}: ${err.message}`);
         }
-
         next();
     }
 }
