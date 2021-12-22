5884bb56b6603603303953ba258d9d3d3dc84a52
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-12-22 17:41:04 +0100

    Added Swagger API docs

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { APIOptions } from './config/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 (async () => {
     const logger = new Logger(`Main`);
     const app = await NestFactory.create<NestExpressApplication>(AppModule, {bodyParser: false});
     app.useWebSocketAdapter(new WsAdapter(app));
     app.disable("x-powered-by");
+
+    const config = new DocumentBuilder()
+        .setTitle("CalenDDar")
+        .setDescription("Multi-platform VTuber API")
+        .setVersion("0.1")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("swagger-api", app, document);
 
     const configService: ConfigService = app.get(ConfigService);
     const apiOptions = configService.get<APIOptions>("api");
