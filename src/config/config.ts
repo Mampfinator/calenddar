4b11388a7eb3bac6e106487c2d73113dbec47575
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-12-23 11:29:31 +0100

    Added basic config verification

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,13 +12,22 @@ export type LoggingOptions = ("log" | "error" | "warn" | "debug" | "verbose")[]
 
 
 export default () => {
+    const verifyEnv = (envConfig: dotenv.DotenvParseOutput, ...fields: string[] ) => {
+        for (const field of fields) if (!envConfig[field]) throw new TypeError(`[.env] ${field} not set!`);
+    }
+
     const envConfig = dotenv.parse(
         readFileSync(join(__dirname,"..", "..", ".env"))
     );
+    verifyEnv(envConfig, "MONGODB_URI", "YOUTUBE_API_KEY", "YOUTUBE_WEBHOOK_SECRET", "TWITCH_API_CLIENT_ID", "TWITCH_API_CLIENT_SECRET", "TWITCH_WEBHOOK_SECRET");
 
     const yamlConfig = yaml.load(
         readFileSync(join(__dirname, `config.${process.env.NODE_ENV ?? "development"}.yaml`), "utf8")
     ) as Record<string, any>
+
+    if (!yamlConfig.api) throw new TypeError("[config file] api options not found!")
+    if (!yamlConfig.api.host) throw new TypeError("[config file] api.host not found!");
+    if (!yamlConfig.api.port) throw new TypeError("[config file] api.port not found!");
 
     return {...envConfig, ...yamlConfig} as Record<string, any>; 
 }
